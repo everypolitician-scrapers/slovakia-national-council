4924dfde80c7b08df2bf7e8b88a34a4cfb7e9297
Tony Bowden <tony@tmtm.com>
2015-09-13 11:00:37 +0100

    add Independents

--- scraper.rb
+++ scraper.rb
@@ -46,11 +46,43 @@ def overlap_old(gmem, cmem, term)
   }
 end
 
+# The source data has no Group memberships for periods where the person
+# was independent. So we need to fill these in with a pseudo-party
+def fill_independents(mems)
+  ind = { name: "Independent", id: "0" }
+
+  if mems.size == 0
+    mems = [ ind ]
+  else
+    mems.unshift ind.merge( end_date: mems.first[:start_date] ) unless mems.first[:start_date].to_s.empty?
+    mems.push    ind.merge( start_date: mems.last[:end_date]  ) unless mems.last[:end_date].to_s.empty?
+  end
+
+  sorted = mems.map do |m| 
+    {
+      id: m[:id],
+      name: m[:name],
+      start_date: m[:start_date].to_s.empty?  ? Date.parse('1000-01-01') : Date.parse(m[:start_date]),
+      end_date: m[:end_date].to_s.empty?  ? Date.parse('9000-12-31') : Date.parse(m[:end_date]),
+    }
+  end.sort_by { |m| m[:start_date] }
+
+  all = sorted.each_cons(2).map do |one, two|
+    gap = ind.merge({ 
+      start_date: one[:end_date] + 1,
+      end_date: two[:start_date] - 1,
+    })
+    gap = nil if gap[:end_date] - gap[:start_date] < 2
+    [one, gap, two]
+  end.flatten.compact.uniq
+
+end
+
 def overlap(mem, term)
-  mS = mem[:start_date].to_s.empty?  ? '0000-00-00' : mem[:start_date]
-  mE = mem[:end_date].to_s.empty?    ? '9999-99-99' : mem[:end_date]
-  tS = term[:start_date].to_s.empty? ? '0000-00-00' : term[:start_date]
-  tE = term[:end_date].to_s.empty?   ? '9999-99-99' : term[:end_date]
+  mS = mem[:start_date].to_s.empty?  ? '0000-00-00' : mem[:start_date].to_s
+  mE = mem[:end_date].to_s.empty?    ? '9999-99-99' : mem[:end_date].to_s
+  tS = term[:start_date].to_s.empty? ? '0000-00-00' : term[:start_date].to_s
+  tE = term[:end_date].to_s.empty?   ? '9999-99-99' : term[:end_date].to_s
 
   return unless mS < tE && mE > tS
   (s, e) = [mS, mE, tS, tE].sort[1,2]
@@ -130,20 +162,20 @@ people.each do |person|
       start_date: gm.xpath('start_date').text,
       end_date: gm.xpath('end_date').text,
     }
-  }
+  }.reject { |tm| tm[:id].to_i == 1 } # no faction information available for term 1
 
-  group_mems = person.xpath('memberships[organization[classification[text()="parliamentary group"]]]').map { |gm|
+  group_mems = fill_independents(person.xpath('memberships[organization[classification[text()="parliamentary group"]]]').map { |gm|
     {
       name: gm.xpath('organization/name').text,
       id: gm.xpath('.//identifiers[scheme[text()="nrsr.sk"]]/identifier').text,
       start_date: latest_date(gm.xpath('organization/founding_date').text, gm.xpath('start_date').text),
       end_date: earliest_date(gm.xpath('organization/dissolution_date').text, gm.xpath('end_date').text),
     }
-  }
+  })
 
   combine(term: term_mems, faction_id: group_mems).each do |mem|
     data = person_data.merge(mem)
-    data[:faction] = groups.find { |g| g[:id] == mem[:faction_id] }[:name]
+    data[:faction] = groups.find(->{{name: 'Independent'}}) { |g| g[:id] == mem[:faction_id] }[:name]
     puts data.to_s.cyan
     ScraperWiki.save_sqlite([:id, :term, :faction_id, :start_date], data.reject { |k,v| v.to_s.empty? })
   end
