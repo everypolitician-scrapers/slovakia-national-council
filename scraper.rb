f45ed39380d8231e72f06f76d2434b41d9a43e94
Tony Bowden <tony@tmtm.com>
2015-09-13 09:28:09 +0100

    Iterate all memberships
    Rather than iterating the terms, and finding the memberships of each,
    change to iterating through all people and finding the combination of
    the term+faction memberships.
    
    Omit term 1 for now as the data isn’t good enough

--- scraper.rb
+++ scraper.rb
@@ -16,6 +16,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 def noko_q(endpoint, h)
   result = RestClient.get (@API_URL % endpoint), params: h
+  warn result.request.url
   doc = Nokogiri::XML(result)
   doc.remove_namespaces!
   entries = doc.xpath('resource/resource')
@@ -23,7 +24,15 @@ def noko_q(endpoint, h)
   return [entries, noko_q(endpoint, h.merge(page: np.text[/page=(\d+)/, 1]))].flatten
 end
 
-def overlap(gmem, cmem, term)
+def earliest_date(*dates)
+  dates.compact.reject(&:empty?).sort.first
+end
+
+def latest_date(*dates)
+  dates.compact.reject(&:empty?).sort.last
+end 
+
+def overlap_old(gmem, cmem, term)
   mS = [gmem[:start_date], gmem[:faction_start], '0000-00-00'].find { |d| !d.to_s.empty? }
   mE = [gmem[:end_date],   gmem[:faction_end],   '9999-99-99'].find { |d| !d.to_s.empty? }
   tS = [cmem[:start_date], term[:start_date], '0000-00-00'].find { |d| !d.to_s.empty? }
@@ -37,11 +46,33 @@ def overlap(gmem, cmem, term)
   }
 end
 
-@mem_id = 0
+def overlap(mem, term)
+  mS = mem[:start_date].to_s.empty?  ? '0000-00-00' : mem[:start_date]
+  mE = mem[:end_date].to_s.empty?    ? '9999-99-99' : mem[:end_date]
+  tS = term[:start_date].to_s.empty? ? '0000-00-00' : term[:start_date]
+  tE = term[:end_date].to_s.empty?   ? '9999-99-99' : term[:end_date]
+
+  return unless mS < tE && mE > tS
+  (s, e) = [mS, mE, tS, tE].sort[1,2]
+  return { 
+    _data: [mem, term],
+    start_date: s == '0000-00-00' ? nil : s,
+    end_date:   e == '9999-99-99' ? nil : e,
+  }
+end
+
+def combine(h)
+  into_name, into_data, from_name, from_data = h.flatten
+  from_data.product(into_data).map { |a,b| overlap(a,b) }.compact.map { |h|
+    data = h.delete :_data
+    h.merge({ from_name => data.first[:id], into_name => data.last[:id] })
+  }.sort_by { |h| h[:start_date] }
+end
+
 
 # http://api.parldata.eu/sk/nrsr/organizations?where={"classification":"chamber"}
-xml = noko_q('organizations', where: %Q[{"classification":"chamber"}] )
-xml.each do |chamber|
+terms = noko_q('organizations', where: %Q[{"classification":"chamber"}] )
+terms.each do |chamber|
   term = { 
     id: chamber.xpath('.//identifiers[scheme[text()="nrsr.sk"]]/identifier').text,
     identifier__parldata: chamber.xpath('.//id').text,
@@ -49,72 +80,72 @@ xml.each do |chamber|
     start_date: chamber.xpath('.//founding_date').text,
     end_date: chamber.xpath('.//dissolution_date').text,
   }
-  puts term
   ScraperWiki.save_sqlite([:id], term, 'terms')
-
-  # http://api.parldata.eu/sk/nrsr/memberships?where={"organization_id":"54d2a70d273a394ad5dbb870"}&embed=["person.memberships.organization"]
-  mems = noko_q('memberships', { 
-    where: %Q[{"organization_id":"#{term[:identifier__parldata]}"}],
-    max_results: 50,
-    embed: '["person.memberships.organization"]'
-  })
-
-  mems.each do |mem|
-    person = mem.xpath('person')
-    person.xpath('changes').each { |m| m.remove } # make eyeballing easier
-    nrsr_id = person.xpath('.//identifiers[scheme[text()="nrsr.sk"]]/identifier').text
-    data = { 
-      id: nrsr_id,
-      identifier__psp: nrsr_id,
-      identifier__parldata: person.xpath('id').text,
-      name: person.xpath('name').text,
-      sort_name: person.xpath('sort_name').text,
-      family_name: person.xpath('family_name').text,
-      given_name: person.xpath('given_name').text,
-      honorific_prefix: person.xpath('honorific_prefix').text,
-      birth_date: person.xpath('birth_date').text,
-      death_date: person.xpath('death_date').text,
-      gender: person.xpath('gender').text,
-      national_identity: person.xpath('national_identity').text,
-      email: person.xpath('email').text,
-      image: person.xpath('image').text,
-      term: term[:id],
-      source: person.xpath('./sources[note[text()="Profil na webe NRSR"]]/url').text,
-    }
-    # data.delete :sort_name if data[:sort_name] == ','
-
-    cmem = {
-      start_date: mem.xpath('start_date').text,
-      end_date: mem.xpath('end_date').text,
-    }
-
-    gmems = person.xpath('memberships[organization[classification[text()="parliamentary group"]]]').map { |gm|
-      {
-        faction: gm.xpath('organization/name').text,
-        faction_id: gm.xpath('.//identifiers[scheme[text()="nrsr.sk"]]/identifier').text,
-        faction_start: gm.xpath('organization/founding_date').text,
-        faction_end: gm.xpath('organization/dissolution_date').text,
-        start_date: gm.xpath('start_date').text,
-        end_date: gm.xpath('end_date').text,
-      }
-    }.select { |gm| overlap(gm, cmem, term) } 
-
-    if gmems.count.zero?
-      row = data.merge({
-        faction: 'Independent', 
-        faction_id: 'IND',
-        mem_id: (@mem_id += 1),
-      })
-      # puts row.to_s.red
-      ScraperWiki.save_sqlite([:mem_id], row)
-    else
-      gmems.each do |gmem|
-        range = overlap(gmem, cmem, term) or raise "No overlap"
-        row = data.merge(gmem).merge(range).merge({ mem_id: (@mem_id += 1) })
-        # puts row.to_s.magenta if gmems.count > 1
-        ScraperWiki.save_sqlite([:mem_id], row)
-      end
-    end
-  end
 end
 
+# http://api.parldata.eu/sk/nrsr/organizations?where={"classification":"parliamentary group"}
+groups = noko_q('organizations', where: %Q[{"classification":"parliamentary group"}] ).map do |group|
+  data = { 
+    id: group.xpath('.//identifiers[scheme[text()="nrsr.sk"]]/identifier').text,
+    identifier__parldata: group.xpath('.//id').text,
+    name: group.xpath('.//name').text,
+    start_date: group.xpath('.//founding_date').text,
+    end_date: group.xpath('.//dissolution_date').text,
+  }
+  ScraperWiki.save_sqlite([:id], data, 'factions')
+  data
+end
+
+# http://api.parldata.eu/sk/nrsr/people?embed=[%22memberships.organization%22]
+people = noko_q('people', { 
+  max_results: 50,
+  embed: '["memberships.organization"]' ,
+})
+
+people.each do |person|
+  person.xpath('changes').each { |m| m.remove } # make eyeballing easier
+  nrsr_id = person.xpath('.//identifiers[scheme[text()="nrsr.sk"]]/identifier').text
+  person_data = { 
+    id: nrsr_id,
+    identifier__nrsr: nrsr_id,
+    identifier__parldata: person.xpath('id').text,
+    name: person.xpath('name').text,
+    sort_name: person.xpath('sort_name').text,
+    family_name: person.xpath('family_name').text,
+    given_name: person.xpath('given_name').text,
+    honorific_prefix: person.xpath('honorific_prefix').text,
+    birth_date: person.xpath('birth_date').text,
+    death_date: person.xpath('death_date').text,
+    gender: person.xpath('gender').text,
+    national_identity: person.xpath('national_identity').text,
+    email: person.xpath('email').text,
+    image: person.xpath('image').text,
+    source: person.xpath('./sources[note[text()="Profil na webe NRSR"]]/url').text,
+  }
+
+  group_mems = person.xpath('memberships[organization[classification[text()="parliamentary group"]]]').map { |gm|
+    {
+      name: gm.xpath('organization/name').text,
+      id: gm.xpath('.//identifiers[scheme[text()="nrsr.sk"]]/identifier').text,
+      start_date: latest_date(gm.xpath('organization/founding_date').text, gm.xpath('start_date').text),
+      end_date: earliest_date(gm.xpath('organization/dissolution_date').text, gm.xpath('end_date').text),
+    }
+  }
+
+  term_mems = person.xpath('memberships[organization[classification[text()="chamber"]]]').map { |gm|
+    {
+      name: gm.xpath('organization/name').text,
+      id: gm.xpath('.//identifiers[scheme[text()="nrsr.sk"]]/identifier').text,
+      start_date: gm.xpath('start_date').text,
+      end_date: gm.xpath('end_date').text,
+    }
+  }.reject { |tm| tm[:id].to_i == 1 } # no faction information available for term 1
+
+  combine(term: term_mems, faction_id: group_mems).each do |mem|
+    data = person_data.merge(mem)
+    data[:faction] = groups.find { |g| g[:id] == mem[:faction_id] }[:name]
+    puts data.to_s.cyan
+    ScraperWiki.save_sqlite([:id, :term, :faction_id, :start_date], data.reject { |k,v| v.to_s.empty? })
+  end
+
+end
